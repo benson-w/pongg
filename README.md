c3d3f357dfd82861518f628d6f5a7a5033497119
benson-w <yuwang15@illinois.edu>
2017-01-02 00:56:21 -0600

    added readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# pong + convo neural net
+
+## Getting Started
+
+### Dependencies
+    * tensorflow
+    * cv2
+    * numpy
+    * random
+    * collections
+    * pygame
+
+### Installation
+    * `python3 learning.py`
+
+Code from https://github.com/malreddysid, made known to me by /||Source||
+    * made minor edits
+    * maybe will add improvements or adaptations later
+    * learning purposes as of now
--- learn.py
+++ learn.py
@@ -76,7 +76,114 @@ def trainGraph(inp, out, sess):
     gt = tf.placeholder("float", [None]) #ground truth
 
     #action 
-    action = tf.reduce_sum(tf.mul(out))
+    action = tf.reduce_sum(tf.mul(out, argmax), reduction_indices = 1)
+    #cost function we will reduce through back propagation
+    cost = tf.reduce_mean(tf.square(action - gt))
+    #optimization function to reduce our cost fn
+    train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
 
+    #initialize our game
+    game = pong.PongGame()
 
+    #create a queue for experience replay to store policies 
+    D = deque()
 
+    #initial frame
+    frame = game.getPresentFrame()
+    #convert rgb to gray scale for processing
+    # also reduce the size from 400, 400 to 84, 84
+    frame = cv2.cvtColor(cv2.resize(frame, (84, 84)), cv2.COLOR_BGR2GRAY)
+    #binary colors, black/white
+    ret, frame = cv2.threshold(frame, 1, 255, cv2.THRESH_BINARY)
+    #stack frames, that is our input tensor 
+    inp_t = np.stack((frame, frame, frame, frame), axis = 2)
+
+    #saver 
+    saver = tf.train.Saver()
+
+    sess.run(tf.initialize_all_variables())
+
+    t = 0
+    epsilon = INITIAL_EPSILON
+
+    #training time
+    while(1):
+        #output tensor       
+        out_t = out.eval(feed_dict = {inp : [inp_t]})[0]
+        #argmax function
+        argmax_t = np.zeros([ACTIONS])
+
+        if (random.random() <= epsilon):
+            maxIndex = random.randrange(ACTIONS)
+        else:
+            maxIndex = np.argmax(out_t)
+        argmax_t[maxIndex] = 1
+
+        if epsilon > FINAL_EPSILON:
+            epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE 
+
+        #reward tensor if score is positive
+        reward_t, frame = game.getNextFrame(argmax_t)
+        #get frame pixel data
+        frame = cv2.cvtColor(cv2.resize(frame, (84, 84)), cv2.COLOR_BGR2GRAY)
+        ret, frame = cv2.threshold(frame, 1, 255, cv2.THRESH_BINARY)
+        frame = np.reshape(frame, (84, 84, 1))
+        #new input tensor 
+        inp_t1 = np.append(frame, inp_t[:, :, 0:3], axis = 2)
+        
+        #add our input tensor argmax tensor, reward and updated input tensor
+        # to stack of experiences 
+
+        D.append((inp_t, argmax_t, reward_t, inp_t1))
+        #if we run out of replay memory, make room
+        if len(D) > REPLAY_MEMORY:
+            D.popleft()
+
+        #training iteration
+        if t > OBSERVE:
+            #get values from our replay memory
+            minibatch = random.sample(D, BATCH)
+
+            inp_batch = [d[0] for d in minibatch]
+            argmax_batch = [d[1] for d in minibatch]
+            reward_batch = [d[2] for d in minibatch]
+            inp_t1_batch = [d[3] for d in minibatch]
+
+            gt_batch = []
+            out_batch = out.eval(feed_dict = {inp : inp_t1_batch})
+
+            #add values to our batch 
+            for i in range(0, len(minibatch)):
+                gt_batch.append(reward_batch[i] + GAMMA * np.max(out_batch[i])
+
+            #train on this
+            train_step.run(feed_dict = {
+                            gt : gt_batch,
+                            argmax : argmax_batch,
+                            inp : inp_batch 
+                            })
+            
+        #update our input tensor with the next frame
+        inp_t = inp_t1
+        t = t + 1
+
+        
+        
+        #print out where we are after saving where we are
+        if t % 10000 == 0
+            saver.save(sess, './' + 'pong' + '-dqn', global_step = t)
+
+        print("TIMESTEP", t, "/ EPSILON", epsilon, "/ ACTION", maxIndex,
+            "/REWARD", reward_t, "/Q_MAX %e" % np.max(out_t))
+
+def main():
+    #create session
+    sess = tf.InteractiveSession()
+    #input layer and output layer by creating graph
+    inp, out = createGraph()
+    #train our graph on input and output with session variables
+    trainGraph(inp, out, sess)
+
+if __name__ == "__main__":
+    main()
+        
